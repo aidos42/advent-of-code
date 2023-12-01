952d6a8bbe7165b6f60d3c82a9d3965c4744d535
Aidos Smagulov <aidos49@yahoo.com>
2023-12-02 05:13:53 +0600

    add 2023 day 1 puzzle

---
+++ 2023/Day-1/index.js
@@ -0,0 +1,51 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const input = (filename) => fs
+  .readFileSync(path.join(__dirname, filename), 'utf8')
+  .toString()
+  .trim()
+  .split('\n');
+
+const calculateSumOfCalibrationValues = (data) => data
+  .map((word) => word.match(/^\d+|\d+\b|\d+(?=\w)/g))
+  .map((numbers) =>  {
+    if (!numbers) {
+      return 0;
+    }
+
+    return Number(numbers[0].charAt(0) + numbers[numbers.length - 1].slice(-1))
+  })
+  .reduce((acc, value) => acc + value);
+
+const inputData = input('input.txt');
+const resultPartOne = calculateSumOfCalibrationValues(inputData);
+
+console.log(resultPartOne);
+
+const wordToDigit = new Map([
+  ['one', '1'],
+  ['two', '2'],
+  ['three', '3'],
+  ['four', '4'],
+  ['five', '5'],
+  ['six', '6'],
+  ['seven', '7'],
+  ['eight', '8'],
+  ['nine', '9'],
+]);
+
+const normalizeCalibrationValues = (data) => data
+  .map((word) => [...word.matchAll(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/g)]
+    .map((match) => match[1]))
+  .map((array) => array.map((value) => wordToDigit.get(value) || value))
+  .map((value) => value.join(''));
+
+const normalizedData = normalizeCalibrationValues(inputData);
+const resultPartTwo = calculateSumOfCalibrationValues(normalizedData);
+
+console.log(resultPartTwo);
